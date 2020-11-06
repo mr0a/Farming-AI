0ed73ca1d5cfd5d8b7088630b06a1786080c354e
Your Name <you@example.com>
2020-11-06 06:04:09 +0000

    Check1

--- project/main.py
+++ project/main.py
@@ -1,12 +1,26 @@
-from flask import Blueprint,render_template
-from . import db
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+from .predict import get_noutput
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_url=request.path)
+
+
+@main.route('/predict', methods=['POST', 'GET'])
+@login_required
+def predict():
+    v1 = float(request.form.get('temperature'))
+    v2 = float(request.form.get('humidity'))
+    v3 = float(request.form.get('ph'))
+    v4 = float(request.form.get('rainfall'))
+    v5 = float(request.form.get('water'))
+    output = get_noutput(3, v1, v2, v3, v4, v5)
+    return render_template('predict.html', context=output)
+
 
 @main.route('/profile')
 @login_required
---
+++ project/predict.py
@@ -0,0 +1,33 @@
+from keras.models import load_model
+from random import randint
+
+model = load_model('/workspace/demologin/project/new_nn.h5')
+
+
+def get_noutput(n_output, temp, hum, ph, rain, water):
+    noutput = []
+    nlabel = []
+    output1 = get_output(temp, hum, ph, rain, water)[0]
+    noutput.append(output1)
+    label1 = get_label(output1)
+    nlabel.append(label1)
+    count = 1
+    while count < n_output:
+        new_output = get_output(temp+randint(-5,5), hum+randint(-20,20), ph+randint(-1,1), rain+randint(-20, 20), water)[0]
+        new_label = get_label(new_output)
+        if new_label not in nlabel:
+            count += 1
+            noutput.append(new_output)
+            nlabel.append(new_label)
+    return nlabel
+
+
+def get_output(temp, hum, ph, rain, water):
+    return model.predict([[temp, hum, ph, rain, water]])
+
+
+def get_label(output):
+    output = list(output)
+    crops = ['rice', 'wheat', 'Mung Bean', 'Tea', 'millet', 'maize', 'Lentil', 'Jute', 'Coffee', 'Cotton', 'Ground Nut', 'Peas', 'Rubber', 'Sugarcane', 'Tobacco', 'Kidney Beans', 'Moth Beans', 'Coconut', 'Black gram', 'Adzuki Beans', 'Pigeon Peas', 'Chickpea', 'banana', 'grapes', 'apple', 'mango', 'muskmelon', 'orange', 'papaya', 'pomegranate', 'watermelon']
+    val = output.index(max(output))
+    return crops[val]
